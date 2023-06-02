f3fc0e66d3cd7cf34f3c1c441eaf2e0a9f915816
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-06-02 18:35:17 -0300

    add comments in the RomanNumbers page

--- src/pages/RomanNumbers/index.js
+++ src/pages/RomanNumbers/index.js
@@ -3,28 +3,33 @@ import { useState } from "react"
 import {romanToArab, arabToRoman, isValidArab, isValidRoman} from "roman-numbers"
 
 export function RomanNumbers() {
-  const [inputValue, setInputValue] = useState("")
-  const [result, setResult] = useState("")
+  const [inputValue, setInputValue] = useState("") // Estado para armazenar o valor de entrada
+  const [result, setResult] = useState("") // Estado para armazenar o resultado da conversão
 
+  // Função para lidar com a alteração do valor de entrada
   function handleInputChange(event) {
     setInputValue(event.target.value)
   }
 
+  // Função para converter de arábico para romano
   function handleArabicToRoman() {
 
+    // Verifica se o valor de entrada é um número arábico válido
     if(!isValidArab(parseInt(inputValue))) {
       setResult("Digite um número inteiro entre 1 e 3999")
     } else {
-      setResult(arabToRoman(parseInt(inputValue)));
+      setResult(arabToRoman(parseInt(inputValue))); // Converte o número arábico para romano
     }
   }
 
+  // Função para converter de romano para arábico
   function handleRomanToArabic() {
 
+    // Verifica se o valor de entrada é um número romano válido
     if(!isValidRoman(inputValue.toUpperCase())) {
       setResult("Digite um número romano válido")
     } else {
-      setResult(romanToArab(inputValue.toUpperCase()))
+      setResult(romanToArab(inputValue.toUpperCase())) // Converte o número romano para arábico
 
     }
   }
