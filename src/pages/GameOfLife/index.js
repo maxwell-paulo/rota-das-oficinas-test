bdb1d4149e8f8fe5fbee4af1b7ab58373157c442
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-06-02 18:28:18 -0300

    add coments in GameOfLife page

--- src/pages/GameOfLife/index.js
+++ src/pages/GameOfLife/index.js
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react"
 
 export function GameOfLife() {
 
-  const [grid, setGrid] = useState(generateEmptyGrid())
-  const [running, setRunning] = useState(false)
+  const [grid, setGrid] = useState(generateEmptyGrid()) // Estado para armazenar o grid
+  const [running, setRunning] = useState(false) // Estado para controlar a simulação
 
+  // Função para gerar um grid vazio
   function generateEmptyGrid() {
     const rows = []
     for (let i = 0; i < 10; i++) {
@@ -13,6 +14,7 @@ export function GameOfLife() {
     return rows
   }
 
+  // Efeito para definir o estado inicial do grid
   useEffect(() => {
     const newGrid = generateEmptyGrid()
     newGrid[3][3] = 1;
@@ -30,20 +32,39 @@ export function GameOfLife() {
     setGrid(newGrid)
   }, [])
 
+  // Efeito para controlar a simulação do Jogo da Vida
   useEffect(() => {
     if (running) {
       const interval = setInterval(() => {
         setGrid((prevGrid) => {
-          return getNextGeneration(prevGrid);
+          const nextGrid = getNextGeneration(prevGrid); // Obter a próxima geração do grid
+        if (isGridEmpty(nextGrid)) {
+          setRunning(false); // Parar a simulação se o grid estiver vazio
+        }
+        return nextGrid;
         });
+        console.log(grid)
       }, 500);
 
       return () => {
-        clearInterval(interval);
+        clearInterval(interval); // Limpar o intervalo quando o componente é desmontado ou quando running muda
       };
     }
   }, [running]);
 
+  // Função para verificar se o grid está vazio
+  function isGridEmpty(grid) {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (grid[i][j] === 1) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // Função para obter a próxima geração do grid
   function getNextGeneration(currGrid) {
     const newGrid = generateEmptyGrid()
 
@@ -68,6 +89,7 @@ export function GameOfLife() {
     return newGrid;
   }
 
+  // Função para contar o número de vizinhos vivos de uma célula
   function countNeighbors(currGrid, row, col) {
     let count = 0;
     const directions = [
@@ -81,10 +103,14 @@ export function GameOfLife() {
       [1, 1],
     ];
 
+    // Percorre todas as direções para verificar os vizinhos
     for (let i = 0; i < directions.length; i++) {
       const [dx, dy] = directions[i];
-      const newRow = row + dx;
-      const newCol = col + dy;
+      const newRow = row + dx; // Coordenada da linha do vizinho
+      const newCol = col + dy; // Coordenada da coluna do vizinho
+
+      // Verifica se as coordenadas estão dentro dos limites do grid
+      // e se o vizinho está vivo (valor igual a 1)
       if (
         newRow >= 0 &&
         newRow < 10 &&
@@ -98,6 +124,7 @@ export function GameOfLife() {
     return count;
   }
 
+  // Função para contar o número de vizinhos vivos de uma célula
   function handleCellClick(i, j) {
     if (!running) {
       const newGrid = [...grid];
@@ -106,14 +133,17 @@ export function GameOfLife() {
     }
   }
 
+  // Função para iniciar a simulação
   function handleStart() {
     setRunning(true);
   }
 
+  // Função para parar a simulação
   function handleStop() {
     setRunning(false)
   }
 
+  // Função para resetar o grid
   function handleReset() {
     setGrid(generateEmptyGrid())
     setRunning(false)
