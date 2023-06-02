62c259e87950a5ddaae466daccd6f292c4f3b9d0
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-06-02 17:19:20 -0300

    create the roman numbers page e logical

--- src/pages/RomanNumbers/index.js
+++ src/pages/RomanNumbers/index.js
@@ -1,6 +1,47 @@
+
+import { useState } from "react"
+import {romanToArab, arabToRoman, isValidArab, isValidRoman} from "roman-numbers"
+
 export function RomanNumbers() {
+  const [inputValue, setInputValue] = useState("")
+  const [result, setResult] = useState("")
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value)
+  }
+
+  function handleArabicToRoman() {
+
+    if(!isValidArab(parseInt(inputValue))) {
+      setResult("Digite um número inteiro entre 1 e 3999")
+    } else {
+      setResult(arabToRoman(parseInt(inputValue)));
+    }
+  }
+
+  function handleRomanToArabic() {
+
+    if(!isValidRoman(inputValue.toUpperCase())) {
+      setResult("Digite um número romano válido")
+    } else {
+      setResult(romanToArab(inputValue.toUpperCase()))
+
+    }
+  }
+
 
   return (
-    <h1>Página dos números romanos</h1>
+
+    <div>
+    <h2>Conversor de números romanos e arábicos</h2>
+    <label>
+      Número:
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </label>
+    <button onClick={handleArabicToRoman}>Converter para Romano</button>
+    <button onClick={handleRomanToArabic}>Converter para Arábico</button>
+    <div>Resultado: {result}</div>
+  </div>
+
   )
 }
