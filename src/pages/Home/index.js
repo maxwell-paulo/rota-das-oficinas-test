d115b63a0d196dc93c5018daf13b8ae6a6257bdd
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-06-03 14:54:43 -0300

    creeate and style the Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,28 @@
+import style from "./style.module.css";
+
 export function Home() {
 
   return (
-    <h1>Tamo na Home</h1>
+    <div className={style.container}>
+    <h1>Teste de Programação - Rota das Oficinas</h1>
+    <p>Nesse site apresento a minha solução para as 3 tarefas do Teste de Programação da Rota das Oficinas.</p>
+    <p>Clique na tarefa que gostaria de acessar</p>
+
+    <ul className={style.taskList}>
+      <li>
+        <a href="/numeros-romanos">Tarefa 1. Conversor de números romanos</a>
+      </li>
+      <li>
+        <a href="/jogo-da-vida">Tarefa 2. Jogo da vida</a>
+      </li>
+      <li>
+        <a href="/conta">Tarefa 3. Divisor de conta de restaurante</a>
+      </li>
+    </ul>
+
+    <p>Para ver o código <a href="https://github.com/maxwell-paulo/rota-das-oficinas-test" target="_blank"
+        rel="noreferrer">clique aqui</a></p>
+  </div>
   )
 
 }
